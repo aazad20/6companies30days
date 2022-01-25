0ba6257d3da2a08b0a302813bb6df92245007d43
Sarvjeet Singh <63469455+jeet4779@users.noreply.github.com>
2022-01-26 00:14:54 +0530

    Create 1.py

---
+++ 5 Intuit/1.py
@@ -0,0 +1,15 @@
+    def minDifference(self, arr, n):
+        s = sum(arr)
+        temp=0
+        if s&1:
+            temp=1
+        s=s//2
+        dp = [ [0 for _ in range(s+1)] for _ in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,s+1):
+                if arr[i-1]<=j:
+                    dp[i][j] = max(arr[i-1]+dp[i-1][j-arr[i-1]],dp[i-1][j])
+                else:
+                    dp[i][j]=dp[i-1][j]
+        ans =dp[n][s]
+        return (2*s+temp-ans)-ans
